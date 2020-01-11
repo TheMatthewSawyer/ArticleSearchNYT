9d5e3ab14ae853aed53a2e825538e5dff0c8775e
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-01-11 14:22:02 -0500

    The javascript does not currently work.

--- javascript.js
+++ javascript.js
@@ -1,45 +1,50 @@
 
+        var query = $("#searchBar").val();
+        var articlesNum = $('#numberRecords option:selected').text();
+        var startYear = $("#startYear").val();
+        var endYear = $("#endYear").val();
+        var filterYear = false;
+        var response1;
+
 $(document).ready(function() {
 
-getArticles();
+    function getArticles() {
 
-function getArticles() {
 
-    var query = $("#searchBar").val();
-    var articlesNum = $('#numberRecords option:selected').text();
-    var startYear = $("#startYear").val();
-    var endYear = $("#endYear").val();
-    var filterYear = false;
+        if(startYear !== null && endYear !== null) {
+            filterYear = true;
+        } else {
+            filterYear = false;
+        }
 
-    if(startYear !== null && endYear !== null) {
-        filterYear = true;
-    } else {
-        filterYear = false;
-    }
-
-    
-
-    $(".searchBtn").click(function() {
-        urlConstructor(filterYear, startYear, endYear, articlesNum, query);
-        $.ajax({
-            url: urlConstructor(),
-            method: "GET"
-        }).then(function(response) {
-            test = response;
-            console.log(test);
+        $(".searchBtn").click(function() {
+            retrieve();
         });
-    });
-}
-
-function urlConstructor(filterYear, startYear, endYear, articlesNum, query){
-    var urlLink = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query;
-    var filter = "pub_year" 
-    if(filterYear) {
-        urlLink += "}&fq={" + filter + "}";
     }
-    urlLink += "&api-key=4KOhVJXzRlJHcH4AXbNod5n1nUpCReUA";
-   return urlLink;
+        function retrieve() {
+            $.ajax({
+                url: urlConstructor(filterYear, startYear, endYear, query),
+                method: "GET"
+            }).then(function(response) {
+                console.log(response);
+                $("#topArticles").html(response.docs[i].headline.main);
 
-}
+            });
+        
+    }
+
+    getArticles();
+
+    function urlConstructor(filterYear, startYear, endYear, query){
+        var urlLink = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query;
+        // var filter = "pub_year";
+        // if(filterYear) {
+        //     urlLink += "}&fq={" + filter + "}";
+        // &begin_date=20120101&end_date=20120101
+        // }
+        urlLink += "&api-key=4KOhVJXzRlJHcH4AXbNod5n1nUpCReUA";
+    return urlLink;
+
+    }
 
 });

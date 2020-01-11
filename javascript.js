4512e8288a140c3762848b1702cfa247aecaf9ef
Matthew Sawyer <57121937+matthewleonardsawyer@users.noreply.github.com>
2020-01-11 13:49:03 -0500

    This

--- javascript.js
+++ javascript.js
@@ -1,27 +1,44 @@
-var test;
 
 $(document).ready(function() {
 
 getArticles();
 
 function getArticles() {
-  console.log("yo");
-    $.ajax({
-        url: "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=4KOhVJXzRlJHcH4AXbNod5n1nUpCReUA",
-        method: "GET"
-    }).then(function(response) {
-        test = response;
-        console.log(test);
+
+    var query = $("#searchBar").val();
+    var articlesNum = $('#numberRecords option:selected').text();
+    var startYear = $("#startYear").val();
+    var endYear = $("#endYear").val();
+    var filterYear = false;
+
+    if(startYear !== null && endYear !== null) {
+        filterYear = true;
+    } else {
+        filterYear = false;
+    }
+
+    
+
+    $(".searchBtn").click(function() {
+        urlConstructor(filterYear, startYear, endYear, articlesNum, query);
+        $.ajax({
+            url: urlConstructor(),
+            method: "GET"
+        }).then(function(response) {
+            test = response;
+            console.log(test);
+        });
     });
 }
 
-function urlConstructor(){
+function urlConstructor(filterYear, startYear, endYear, articlesNum, query){
     var urlLink = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + query;
+    var filter = "pub_year" 
     if(filterYear) {
         urlLink += "}&fq={" + filter + "}";
     }
     urlLink += "&api-key=4KOhVJXzRlJHcH4AXbNod5n1nUpCReUA";
-   
+   return urlLink;
 
 }
 
